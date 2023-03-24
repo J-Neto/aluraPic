d7dd1314d07b07d8df26058b6ce325a4160843c3
José Neto <ribeirojoseph44@gmail.com>
2023-03-24 17:21:46 -0300

    NgFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  photos = [
+    {
+      url: "https://feijoadasimulator.top/br/sources/2932.jpeg",
+      description: "Nando Moura careca"
+    },
+    {
+      url: "https://images4.alphacoders.com/936/936378.jpg",
+      description: "Universo"
+    }
+  ]
+
 }
