718dbc405330f2984c53412c3d59a38d2219ac0e
José Neto <ribeirojoseph44@gmail.com>
2023-03-29 09:26:25 -0300

    :recycle: refactor: Create submodules

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -1,4 +1,7 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { PhotoModule } from '../photo/photo.module';
+import { FilterByDescription } from './filter-by-description.pipe';
 import { LoadButtonComponent } from './load-button/load-button.component';
 import { PhotoListComponent } from './photo-list.component';
 import { Photos1Component } from './photos1/photos1.component';
@@ -7,7 +10,12 @@ import { Photos1Component } from './photos1/photos1.component';
     declarations: [
         PhotoListComponent,
         Photos1Component,
-        LoadButtonComponent
+        LoadButtonComponent,
+        FilterByDescription,        
+    ],
+    imports: [
+        CommonModule,
+        PhotoModule
     ]
 })
 export class PhotoListModule {}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -8,19 +8,15 @@ import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { Photos1Component } from './photo-list/photos1/photos1.component';
 import { FilterByDescription } from './photo-list/filter-by-description.pipe';
 import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoModule } from './photo/photo.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
 
 @NgModule({
-    declarations: [ 
-        PhotoComponent, 
-        PhotoListComponent, 
-        PhotoFormComponent, 
-        Photos1Component, 
-        FilterByDescription, 
-        LoadButtonComponent
-    ],
     imports: [ 
-        HttpClientModule, 
-        CommonModule 
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule,
     ]
 })
 export class PhotosModule {}
