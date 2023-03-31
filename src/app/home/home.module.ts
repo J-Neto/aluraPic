6aebf41db40d12de9166af4a50b4d4b3ad6cf07c
José Neto <ribeirojoseph44@gmail.com>
2023-03-31 12:12:50 -0300

    :recycle: refactor: componetize input error message

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/signin.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { VMessageComponent } from '../shared/components/vmessage/vmessage.component';
 
 @NgModule({
     declarations: [ SignInComponent ],
     imports: [ 
         CommonModule, 
-        ReactiveFormsModule 
+        ReactiveFormsModule,
+        VMessageModule
     ]
 })
 export class HomeModule {}
