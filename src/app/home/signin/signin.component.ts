99d0ea6218cc82c415918a3f91129a0efb9b1d04
José Neto <ribeirojoseph44@gmail.com>
2023-03-31 11:34:59 -0300

    :sparkles: feat: login form

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,6 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+export interface Login {
+  userName: string;
+  password: string;
+}
 
 @Component({
-    templateUrl: './signin.component.html'
+  templateUrl: './signin.component.html',
 })
-export class SignInComponent {}
+export class SignInComponent implements OnInit {
+  
+  loginForm!: FormGroup<any>;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+}
